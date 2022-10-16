d5ff588ec16e51c77f892fb3c293c0380c6f5ddf
LimaFrancisco <francisco1111.fl@protonmail.com>
2022-10-16 00:43:57 -0300

    modified hello.c and hello

--- hello.c
+++ hello.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> //O include inclui um arquivo pronto ao programa, nesse caso a biblioteca stdio.h
+//main eh a funcao principal e obrigatoria para o funcionamento da funcao
+int main(void){ //int = tipo de retorno da funcao, main = nome da funcao, (void) = entrada da funcao
+   int ret = 0;
 
-int main(void){
-	printf("papobinario.com.br\n");
-   printf("Vejo vcs na aula 1\n");
-	return 0;
+   printf("papobinario.com.br\n"); //funcao do sdtio.h
+   printf("%c", 10);
+   printf("%x - %x - %x - %x\n", ret, 10, 0xa, 0x41); 
+	return 0; // a funcao retorna o numero inteiro de valor 0
 }
